388b65fad62e316030ac4c031cd361722b274558
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-21 18:45:29 -0500

    Changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser'); 
 var mongoose = require('mongoose');
-var port = 80;
+var port = process.env.PORT || 80;
 
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.url,{ useNewUrlParser: true }); //Connect to the database
@@ -10,5 +10,5 @@ var app = express();
 app.use(bodyParser.json([{strict: false }]));
 
 require('./routes.js')(app);
-app.listen(6007);
+app.listen(port);
 console.log("Project Haven Server now listening on port " + port);
