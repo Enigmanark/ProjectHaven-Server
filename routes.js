ae4c04b3c8e65002ad733c422ddf05dce67da3dc
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-21 16:23:41 -0500

    Characters are now created properly and sent to the client

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 var signup = require("./signup");
 var Player = require("./Models/player");
 var login = require("./config/login");
+var characterJSON = require("./characterJSON");
 
 module.exports = function(app, passport) {
     app.get("/", function(req, res) {
@@ -14,67 +15,91 @@ module.exports = function(app, passport) {
     }
     );
 
-    app.get("/getcharacter", login, function(req, res) {
+    app.post("/makecharacter", login, function(req, res) {
         console.log("Looking for player's account..");
         Player.findOne( { "email" : req.body.Email }, function(err, player) {
             if(player) {
+                console.log("Found player's account");
                 if(player.validPassword(req.body.Password)) {
-                    console.log("Found account, now sending character data");
-                    var character = {
-                        "Name" : "Solar",
-                        "Level" : 100,
-                        "Experience" : 0,
-                        "Gold" : 0,
-                        "ExperienceToLevelUp" : 100,
-                        "TrainingPoints" : 0,
-                        "BaseHP" : 50,
-                        "MaxHP" : 50,
-                        "CurrentHP" : 50,
-                        "BaseSP" : 15,
-                        "MaxSP" : 15,
-                        "CurrentSP" : 15,
-                        "BaseMP" : 0,
-                        "MaxMP" : 0,
-                        "CurrentMP" : 0,
-                        "Strength" : 0,
-                        "CurrentStrength" : 0,
-                        "Dexterity" : 0,
-                        "CurrentDexterity" : 0,
-                        "Endurance" : 0,
-                        "CurrentEndurance" : 0,
-                        "Intelligence" : 0,
-                        "CurrentIntelligence" : 0,
-                        "Willpower" : 0,
-                        "CurrentWillpower" : 0,
-                        "Agility" : 0,
-                        "CurrentAgility" : 0,
-                        "MeleeDef" : 0,
-                        "CurrentMeleeDef" : 0,
-                        "RangedDef" : 0,
-                        "CurrentRangedDef" : 0,
-                        "SpellDef" : 0,
-                        "CurrentSpellDef" : 0,
-                        "Earth" : .92,
-                        "CurrentEarth" : .92,
-                        "Water" : .92,
-                        "CurrentWater" : .92,
-                        "Air" : .92,
-                        "CurrentAir" : .92,
-                        "Fire" : .92,
-                        "CurrentFire" : .92,
-                        "Ice" : .92,
-                        "CurrentIce" : .92,
-                        "Thunder" : .92,
-                        "CurrentThunder" : .92,
-                        "Light" : .92,
-                        "CurrentLight" : .92,
-                        "Dark" : .92,
-                        "CurrentDark" : .92,
-                        "CurrentWeaponID" : 1,
+                    console.log("Password is valid!");
+                    var characters = player.characters;
+                    if(characters.length == 5) {
+                        res.send("600");
                     }
-                    character = JSON.stringify(character);
-                    res.send(character);
-                    console.log("Character data sent");
+                    else {
+                        console.log("Attempting to create character");
+                        var charData = characterJSON;
+                        charData["Name"] = req.body.Name;
+                        characters.push(charData);
+                        player.save(function(err) {
+                            if(err) {
+                                throw err;
+                            }
+                            console.log("Character created!");
+                            res.send("500");
+                        })
+                    }
+                }
+                else {
+                    res.send("300");
+                }
+            }
+            else {
+                res.send("300");
+            }
+        });
+    });
+
+    app.post("/choosecharacter", login, function(req, res) {
+        console.log("Looking for player's account..");
+        Player.findOne( { "email" : req.body.Email }, function(err, player) {
+            if(player) {
+                console.log("Found player's account");
+                if(player.validPassword(req.body.Password)) {
+                    console.log("Password is valid!");
+                    var characters = player.characters;
+                    var data = JSON.stringify(characters);
+                    console.log("Now sending characters..");
+                    res.send(data);
+                }
+                else {
+                    res.send("300");
+                }
+            }
+            else {
+                res.send("300");
+            }
+        });
+    });
+
+    app.post("/getcharacter", login, function(req, res) {
+        console.log("Looking for player's account..");
+        Player.findOne( { "email" : req.body.Email }, function(err, player) {
+            if(player) {
+                console.log("Found account");
+                if(player.validPassword(req.body.Password)) {
+                    console.log("Valid password!");
+                    console.log("Looking for character..");
+                    var charName = req.body.Name;
+                    var characterData = player.characters;
+                    var chosenCharacter;
+                    for(i = 0; i < characterData.length; i++) {
+                        var n = characterData[i]["Name"];
+                        if(n == charName) {
+                            console.log("Found character!");
+                            chosenCharacter = characterData[i];
+                            break;
+                        }
+                    }
+                    if(chosenCharacter == null) {
+                        console.log("Could not find character :^(");
+                        res.send("400");
+                    }
+                    else {
+                        chosenCharacter = JSON.stringify(chosenCharacter);
+                        res.send(chosenCharacter);
+                        console.log("Character sent!");
+                    }
                 }
                 else {
                     res.send("300");
