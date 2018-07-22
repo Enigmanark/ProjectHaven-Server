bc037ed8bf6614a503f4c417ddecb6b5c67ab72e
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-22 14:32:40 -0500

    Added elementl weapons to starter for testing

--- inventoryJSON.js
+++ inventoryJSON.js
@@ -1,13 +1,13 @@
 module.exports = function() {
     var weps = [];
 	weps.push(1);
-	weps.push(null);
-	weps.push(null);
-	weps.push(null);
-	weps.push(null);
-	weps.push(null);
-	weps.push(null);
-	weps.push(null);
+	weps.push(2);
+	weps.push(3);
+	weps.push(4);
+	weps.push(5);
+	weps.push(6);
+	weps.push(7);
+	weps.push(8);
 	weps.push(null);
 	weps.push(null);
 	var inv = {
