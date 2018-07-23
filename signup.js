ac500f253fb44f0558be34f191e06aa6d542b757
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-23 18:17:11 -0500

    Second attempt at trying to fix account problem

--- signup.js
+++ signup.js
@@ -8,16 +8,17 @@ module.exports = function(req, res, next) {
     //Check if there's an account already with that email
     Player.findOne( { "email" : email}, function(err, user) {
         if(err) throw err;
-        if(user) { //If we found a user, then signup fails, redirect
+        if(user) { //If we found a user, then signup fails
             res.send("100");
             return false;
         }
-    });
-
-    var newUser = new Player(); //Make a new user and set it's fields
-    newUser.email = email; //set the email
-    newUser.password = newUser.generateHash(password); //encrypt the password
-    newUser.save(function(err) { //save the user
-        return next(); //once we reach here, we're finished so return next()
+        else {
+            var newUser = new Player(); //Make a new user and set it's fields
+            newUser.email = email; //set the email
+            newUser.password = newUser.generateHash(password); //encrypt the password
+            newUser.save(function(err) { //save the user
+                return next(); //once we reach here, we're finished so return next()
+            });
+        }
     });
 };
