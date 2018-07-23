306cb3974cf3e8fccca52e7f226d75cfc02ebd2e
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-23 08:21:13 -0500

    Attempt to fix crash again

--- updateVitals.js
+++ updateVitals.js
@@ -2,21 +2,17 @@ var healthEnduranceMod = 10;
 var manaIntelligenceMod = 15;
 var staminaEnduranceMod = 2;
 
-function updateHealth(stats) {
+exports.updateHealth = function(stats) {
     stats["MaxHP"] = stats["BaseHP"] + (stats["Endurance"] * healthEnduranceMod);
     return stats;
 };
 
-function updateMana(stats) {
+exports.updateMana = function(stats) {
     stats["MaxMP"] = stats["BaseMP"] + (stats["Intelligence"] * manaIntelligenceMod);
     return stats;
 };
 
-function updateStamina(stats) {
+exports.updateStamina = function(stats) {
     stats["MaxSP"] = stats["BaseSP"] + (stats["Endurance"] * staminaEnduranceMod);
     return stats;
 };
-
-exports = updateHealth();
-exports = updateMana();
-exports = updateStamina();
