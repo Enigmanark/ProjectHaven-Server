667acc0b2abbe0d2e151189e04f0e0468022943c
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-23 22:00:59 -0500

    Test 5

--- Enemies/enemyLocations.js
+++ Enemies/enemyLocations.js
@@ -1,14 +1,14 @@
 module.exports = function() {
     var locations = [];
-		locations.push["Forest"];
-		locations.push["Dungeon"];
-		locations.push["Plains"];
-		locations.push["Volcano"];
-		locations.push["Glacier"];
-		locations.push["Dark Forest"];
-		locations.push["Desert"];
-		locations.push["Beach"];
-		locations.push["Mountains"];
+		locations.push("Forest");
+		locations.push("Dungeon");
+		locations.push("Plains");
+		locations.push("Volcano");
+		locations.push("Glacier");
+		locations.push("Dark Forest");
+		locations.push("Desert");
+		locations.push("Beach");
+		locations.push("Mountains");
     
     return locations;
 };
