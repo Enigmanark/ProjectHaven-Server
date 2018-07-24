1074371a3f592a509c41d02c327c212ac68f087d
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-23 21:41:02 -0500

    I derped

--- Routes/routes_battle.js
+++ Routes/routes_battle.js
@@ -2,7 +2,7 @@ var randomBattle = require("../Battle/randomBattle");
 var login = require("../config/login");
 
 module.exports = function(app) {
-    app.get("/randombattle", login, function(req, res) {
+    app.post("/randombattle", login, function(req, res) {
         randomBattle();
     });
 };
