f3d30c7136b5f146101a73213a736f2516de3b73
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-23 21:49:55 -0500

    Test 2

--- Battle/randomBattle.js
+++ Battle/randomBattle.js
@@ -4,21 +4,26 @@ var Enemy = require("../Models/enemy");
 module.exports = function() {
     var rand = Math.floor(Math.random() * locations.length);
     var randLoc = locations[rand];
-
+    console.log(randLoc);
     Enemy.find({}).lean().where('Locations').in([randLoc]).limit(30).exec(function(err, en) {
         if(err) {
             console.log("Query error or database offline or something");
             res.send("801");
         }
         else if(en){
-            var enemies = en;
-            rand = Math.floor(Math.random() * enemies.length);
-            var battle = enemies[rand];
-            battle = JSON.stringify(battle);
-            console.log(battle);
-            battle["BattleLocation"] = randLoc;
-            console.log("Sending battle data");
-            res.send(battle);
+            if(en.size != 0) {
+                var enemies = en;
+                rand = Math.floor(Math.random() * enemies.length);
+                var battle = enemies[rand];
+                battle = JSON.stringify(battle);
+                battle["BattleLocation"] = randLoc;
+                console.log("Sending battle data");
+                res.send(battle);
+            }
+            else {
+                console.log("Couldn't find a random battle with those search queries");
+                res.send("802");
+            }
         }
         else {
             console.log("Couldn't find a random battle with those search queries");
