41b89193b06eff374dfde41dd93b2449a66d381b
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-26 15:52:57 -0500

    Made some functions async

--- CharacterData/GetEquipmentData.js
+++ CharacterData/GetEquipmentData.js
@@ -6,7 +6,7 @@ module.exports = async function(req, res, character) {
     var wepArray = character["Inventory"]["Weapons"];
     await Weapon.find({}).where('ID').in([wepArray[0], wepArray[1],
         wepArray[2], wepArray[3], wepArray[4], wepArray[5], wepArray[6], 
-        wepArray[7], wepArray[8], wepArray[9], ]).exec(function(err, weapons) {
+        wepArray[7], wepArray[8], wepArray[9], ]).exec(async function(err, weapons) {
 		if(err) {
             console.log("Got a database error in GetWeaponData");
             throw err;
@@ -36,7 +36,7 @@ module.exports = async function(req, res, character) {
             var armorArray = character["Inventory"]["Armors"];
             await Armor.find({}).where('ID').in([armorArray[0], armorArray[1],
                armorArray[2], armorArray[3], armorArray[4], armorArray[5], armorArray[6], 
-                armorArray[7], armorArray[8], armorArray[9], ]).exec(function(err, armors) {
+                armorArray[7], armorArray[8], armorArray[9], ]).exec(async function(err, armors) {
                 if(err) {
                     console.log("Got a database error in GetArmorData");
                     throw err;
@@ -66,7 +66,7 @@ module.exports = async function(req, res, character) {
                     var shieldArray = character["Inventory"]["Shields"];
                     await Shield.find({}).where('ID').in([shieldArray[0], shieldArray[1],
                        shieldArray[2], shieldArray[3], shieldArray[4], shieldArray[5], shieldArray[6], 
-                        shieldArray[7], shieldArray[8], shieldArray[9], ]).exec(function(err, shields) {
+                        shieldArray[7], shieldArray[8], shieldArray[9], ]).exec(async function(err, shields) {
                         if(err) {
                             console.log("Got a database error in GetShieldData");
                             throw err;
