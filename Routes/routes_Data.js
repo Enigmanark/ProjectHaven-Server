cdf84bbc2ce43c641c10853dbab8db1765fa1ddc
Johnathan Crocker <johnathan.crocker@gmail.com>
2018-07-27 16:41:51 -0500

    Fixed derp

--- Routes/routes_Data.js
+++ Routes/routes_Data.js
@@ -3,7 +3,7 @@ var login = require("../config/login");
 
 module.exports = function(app) {
 
-    app.get("/gethavenblacksmithinventory", login, function(req, res) {
+    app.post("/gethavenblacksmithinventory", login, function(req, res) {
         blacksmith(req, res);
     });
 }
